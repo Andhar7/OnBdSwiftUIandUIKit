afcbe8fd1bd7bec1ec3c2cff2aa08fdb5cae93d7
Andrej Kling <boxingprofi@web.de>
2021-01-23 18:38:26 +0100

    Initial Commit

---
+++ OnBdSwiftUIandUIKit/OnBdSwiftUIandUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBdSwiftUIandUIKitApp.swift
+//  OnBdSwiftUIandUIKit
+//
+//  Created by Andrej Kling on 23.01.21.
+//
+
+import SwiftUI
+
+@main
+struct OnBdSwiftUIandUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
